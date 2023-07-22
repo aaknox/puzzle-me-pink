7aa0ecaf5aa61c742d5b662c14d16df923503518
aaknox <azhya.knox@gmail.com>
2023-07-22 12:09:44 -0500

    updated gitignore, added backend skeleton code

---
+++ pmp-backend/src/main/java/com/azhya/PmpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.azhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmpBackendApplication.class, args);
+	}
+
+}
